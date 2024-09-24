99e6223af065112c2f639f420d4e8a2a825bcd52
fmsbbb <yapurivinod@gmail.com>
2024-09-24 12:19:13 +0530

    breaking: my breaking change

--- release.config.js
+++ release.config.js
@@ -1,34 +1,18 @@
-const config = {
-  branches: ['main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    {
-      "parser": "@semantic-release/commit-parser",
-      "parserOpts": {
-        "headerPattern": /^(\w+): (.*)$/,
-        "headerCorrespondence": ["type", "subject"],
-        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"],
-        "noteKeywordsRegex": [/BREAKING CHANGE/i, /BREAKING CHANGES/i],
-        "bodyPattern": /^BREAKING CHANGE: (.*)$/i
-      },
-      "releaseRules": [
-        { "type": "fix", "release": "patch" },
-        { "type": "feat", "release": "minor" },
-        { "type": "perf", "release": "patch" },
-        { "type": /^breaking:.*$/i, "release": "major", "increment": true, "breaking": true },
-        { "type": /^breaking$/i, "release": "major", "increment": true, "breaking": true },
-        { "type": /^breaking:.*$/i, "release": "major", "increment": true, "breaking": true, "path": ".*" },
-        { "type": /^BREAKING:.*$/i, "release": "major", "increment": true, "breaking": true },
-        { "type": /^BREAKING$/i, "release": "major", "increment": true, "breaking": true },
-        { "type": /^BREAKING CHANGE:.*$/i, "release": "major", "increment": true, "breaking": true },
-        { "type": /^BREAKING CHANGE$/i, "release": "major", "increment": true, "breaking": true }
-      ],
-      "debug": true
-    },
+{
+  "branches": ["main", "next"],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/git",
-    '@semantic-release/github'
-  ] // <--- This bracket was missing
-};
-
-module.exports = config;
+    "@semantic-release/github"
+  ],
+  "preset": "angular",
+  "tagFormat": "${version}",
+  "github": {
+    "assets": [
+      {
+        "path": "dist/**/*.{js,css,map}",
+        "name": "dist"
+      }
+    ]
+  }
+}
